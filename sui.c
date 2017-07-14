59a1eada78ee1a339fdffccb5cade4acbd6bd7b4
Hui <bugway@gmail.com>
2017-07-14 15:36:24 +0900

    untabify

--- sui.c
+++ sui.c
@@ -44,9 +44,9 @@ extern "C" {
     
 static int
 getticks() {
-	struct timespec ts;
-	clock_gettime(CLOCK_MONOTONIC, &ts);
-	return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
 }
 
 typedef struct {
